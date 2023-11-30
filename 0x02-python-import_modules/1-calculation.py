3fe331b1b4e415fc026cda77a4c8c7c1319a4aff
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-11-30 21:13:33 +0100

    Import functions from the file calculator_1.py, does some Maths, and prints the result

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+a = 10
+b = 5
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
+
+    
