d8e9e724894c19a12938d022e9d3a698d6f81b1b
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-11-30 22:08:51 +0100

    Print the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for i in sys.argv:
+        if i != sys.argv[0]:
+            result = result + int(i)
+    print(result)
+        
