110320058fc99163683b7f63d66dde4b8a2094ed
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-11-30 23:08:26 +0100

    Import all functions from the file calculator_1.py and handles basic operations

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    def my_calculator():
+        if len(sys.argv) != 4:
+            print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+            sys.exit(1)
+
+        a = int(sys.argv[1])
+        operator = sys.argv[2]
+        b = int(sys.argv[3])
+
+        result = 0
+
+        if operator == '+':
+            result = add(a, b)
+        elif operator == '-':
+            result = sub(a, b)
+        elif operator == '*':
+            result = mul(a, b)
+        elif operator == '/':
+            result = div(a, b)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+
+        print("{} {} {} = {}".format(a, operator, b, result))
+
+    if __name__ == "__main__":
+        my_calculator()
