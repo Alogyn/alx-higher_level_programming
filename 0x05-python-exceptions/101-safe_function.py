041a176b425ec697cdf07d9cca2dd2e1927b756b
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-12-18 23:18:03 +0100

    Execute a function safely

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
-        try:
-            if i > a:
-                raise Exception('Too far')
-            result += a ** b / i
-        except Exception:
-            result = b + a
-            break
-    return (result)
+def safe_function(fct, *args):
+    try:
+        r = fct(*args)
+        return (r)
+    except Exception as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+        return (None)
