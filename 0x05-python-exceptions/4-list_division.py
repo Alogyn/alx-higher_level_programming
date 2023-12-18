46dd06da2f5290428cb4c8b47a798d90b7d6195e
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-12-18 21:59:16 +0100

    Divide element by element 2 lists

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            quotient = my_list_1[i] / my_list_2[i]
+        except (TypeError, ValueError):
+            print("wrong type")
+            quotient = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            quotient = 0
+        except IndexError:
+            print("out of range")
+            quotient = 0
+        finally:
+            result.append(quotient)
+    return (result)
