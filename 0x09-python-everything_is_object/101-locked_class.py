a68323c6fdeb283aa34b8760ed425adc5714953f
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-01-05 23:26:03 +0100

    Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,4 +4,8 @@
 
 
 class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
     __slots__ = ['first_name']
