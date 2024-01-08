8c9987431a83ea63a1ccb7b39f36144f75d2b2ad
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-01-08 05:23:11 -0800

    Multiplie 2 matrices

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -62,10 +62,10 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_b should contain only integers or floats")
 
     if m_a_notrect:
-        raise TypeError("each row of m_a must be of the same size")
+        raise TypeError("each row of m_a must should be of the same size")
 
     if m_b_notrect:
-        raise TypeError("each row of m_b must be of the same size")
+        raise TypeError("each row of m_b must should be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
