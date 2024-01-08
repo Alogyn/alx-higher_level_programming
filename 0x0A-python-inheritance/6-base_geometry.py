fa1a7018d486bac21998fdf5ad77a4d337da1de9
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-01-08 23:46:00 +0100

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Module for BaseGeometry class, area function.
+'''
+
+
+class BaseGeometry:
+    """Empty class BaseGeometry"""
+
+    def area(self):
+        """Raises an Exception with the message area() is not implemented."""
+        raise Exception("area() is not implemented")
