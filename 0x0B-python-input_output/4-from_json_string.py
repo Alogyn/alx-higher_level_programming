95283c38fb2224d70191a234dc2049626a854e5d
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-01-09 23:59:50 +0100

    Return an object (Python data structure) represented by a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for from_json_string function.
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string.
+
+    Returns:
+        object: The Python data structure represented by the JSON string.
+    """
+    return json.loads(my_str)
