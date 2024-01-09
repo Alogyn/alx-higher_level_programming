cb0eb62a1ec9d2219e9d6e0e8d02f5a3dcd22d72
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-01-09 23:50:04 +0100

    Return the JSON representation of an object (string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for to_json_string function.
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj: The object to be converted to JSON.
+
+    Returns:
+        str: The JSON representation of the object.
+    """
+    return json.dumps(my_obj)
