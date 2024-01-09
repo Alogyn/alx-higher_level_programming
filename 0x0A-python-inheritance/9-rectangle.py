ec65d559c1a3e2a6dd563987462db2565f126609
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-01-09 02:04:31 +0100

    Inherit from BaseGeometry (7-base_geometry.py). (task based on 8-rectangle.py)

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+'''
+Module for BaseGeometry class, area function & integer_validator function.
+Module for Rectangle class, __init__ function & area function,
+__str__ function.
+'''
+
+
+class BaseGeometry:
+    """Empty class BaseGeometry"""
+
+    def area(self):
+        """Raises an Exception with the message area() is not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value as an integer greater than 0.
+
+        Args:
+            name (str): The name of the value.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Class Rectangle that inherits from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Instantiates a Rectangle with width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Computes the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns the string representation of the rectangle.
+
+        Returns:
+            str: The rectangle description.
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
