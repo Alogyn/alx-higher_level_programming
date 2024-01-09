5a8f522231d75ec1ce735bfc9e4ac6a3c6c35bea
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-01-10 00:53:50 +0100

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module for Student class.
+"""
+
+
+class Student:
+    """Class that defines a student."""
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: Dictionary representation of the Student instance.
+        """
+        return self.__dict__
