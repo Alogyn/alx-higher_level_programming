bb5a6f17ba658947cb34791b95c93f4e88314ae5
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-01-13 23:56:21 +0100

    Add attribute validation to Rectangle class: integer type check and value range checks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Rectangle module"""
-
+""" Module for Rectangle class """
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Rectangle class"""
+    """ Rectangle class inherited from Base """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Constructor method"""
+        """ Initializes a Rectangle instance """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -17,60 +16,56 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Getter for width"""
+        """ Gets the width of the rectangle """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter for width"""
-        if not isinstance(value, int):
+        """ Sets the width of the rectangle """
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value <= 0:
             raise ValueError("width must be > 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
-        """Getter for height"""
+        """ Gets the height of the rectangle """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter for height"""
-        if not isinstance(value, int):
+        """ Sets the height of the rectangle """
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value <= 0:
             raise ValueError("height must be > 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     @property
     def x(self):
-        """Getter for x"""
+        """ Gets the x-coordinate of the rectangle """
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Setter for x"""
-        if not isinstance(value, int):
+        """ Sets the x-coordinate of the rectangle """
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         elif value < 0:
             raise ValueError("x must be >= 0")
-        else:
-            self.__x = value
+        self.__x = value
 
     @property
     def y(self):
-        """Getter for y"""
+        """ Gets the y-coordinate of the rectangle """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Setter for y"""
-        if not isinstance(value, int):
+        """ Sets the y-coordinate of the rectangle """
+        if type(value) is not int:
             raise TypeError("y must be an integer")
         elif value < 0:
             raise ValueError("y must be >= 0")
-        else:
-            self.__y = value
+        self.__y = value
