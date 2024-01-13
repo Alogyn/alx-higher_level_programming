4cd1f1a129e81a62e882e3127ca0173038a024ab
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-01-14 00:31:45 +0100

    Add public getter and setter for Square size attribute

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,18 @@
-# models/rectangle.py
-
+#!/usr/bin/python3
+"""
+models/rectangle.py
+"""
 from models.base import Base
 
+
 class Rectangle(Base):
+    """Rectangle class inherits from Base class."""
+    
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize the Rectangle instance."""
+        # Call the superclass constructor
         super().__init__(id)
+        # Set attributes with provided values
         self.width = width
         self.height = height
         self.x = x
@@ -12,10 +20,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Getter method for width."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Setter method for width with validation."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value <= 0:
@@ -24,10 +34,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """Getter method for height."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Setter method for height with validation."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value <= 0:
@@ -36,10 +48,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """Getter method for x."""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """Setter method for x with validation."""
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         elif value < 0:
@@ -48,10 +62,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """Getter method for y."""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """Setter method for y with validation."""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         elif value < 0:
@@ -59,18 +75,22 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """Calculate and return the area of the rectangle."""
         return self.__width * self.__height
 
     def display(self):
+        """Display the rectangle using '#' characters, accounting for x and y offsets."""
         for _ in range(self.__y):
             print()
         for _ in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
+        """Return a string representation of the rectangle."""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
+        """Update attributes using either positional arguments or keyword arguments."""
         if args:
             attrs = ["id", "width", "height", "x", "y"]
             for i in range(len(args)):
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,19 +1,40 @@
-# models/square.py
+"""models/square.py"""
 
 from models.rectangle import Rectangle
 
 class Square(Rectangle):
+    """Square class inherits from Rectangle class."""
+
     def __init__(self, size, x=0, y=0, id=None):
+        """Initialize the Square instance."""
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """Getter method for size."""
         return self.width
 
     @size.setter
     def size(self, value):
+        """Setter method for size."""
         self.width = value
         self.height = value
 
-    def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+"""10-main.py"""
+
+#!/usr/bin/python3
+""" 10-main """
+from models.square import Square
+
+if __name__ == "__main__":
+
+    s1 = Square(5)
+    print(s1)
+    print(s1.size)
+    s1.size = 10
+    print(s1)
+
+    try:
+        s1.size = "9"
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
