a04da352561565417d36312928bd1522e9b3303a
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-01-14 00:16:31 +0100

    Improve display method in Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -62,8 +62,10 @@ class Rectangle(Base):
         return self.__width * self.__height
 
     def display(self):
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
