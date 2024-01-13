23b8d746a8270b685fc57ed965ac1731e3765dd5
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-01-14 00:09:32 +0100

    Add display method to Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,9 @@
-#!/usr/bin/python3
-""" Module for Rectangle class """
+# models/rectangle.py
+
 from models.base import Base
 
-
 class Rectangle(Base):
-    """ Rectangle class inherited from Base """
-
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ Initializes a Rectangle instance """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -16,13 +12,11 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """ Gets the width of the rectangle """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ Sets the width of the rectangle """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value <= 0:
             raise ValueError("width must be > 0")
@@ -30,13 +24,11 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """ Gets the height of the rectangle """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ Sets the height of the rectangle """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value <= 0:
             raise ValueError("height must be > 0")
@@ -44,13 +36,11 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """ Gets the x-coordinate of the rectangle """
         return self.__x
 
     @x.setter
     def x(self, value):
-        """ Sets the x-coordinate of the rectangle """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("x must be an integer")
         elif value < 0:
             raise ValueError("x must be >= 0")
@@ -58,18 +48,19 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """ Gets the y-coordinate of the rectangle """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """ Sets the y-coordinate of the rectangle """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("y must be an integer")
         elif value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
-        """ Returns the area value of the Rectangle instance """
         return self.__width * self.__height
+
+    def display(self):
+        for _ in range(self.__height):
+            print("#" * self.__width)
