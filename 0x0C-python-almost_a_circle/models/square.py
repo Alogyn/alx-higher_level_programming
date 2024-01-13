1304e3eaac4a529b98e14ac9d687a44fbee318a8
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-01-14 00:26:12 +0100

    Add Square class inheriting from Rectangle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+# models/square.py
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
