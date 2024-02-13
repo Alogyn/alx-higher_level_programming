688f11c0d987781c94ff21c7bc75fd80bee6da55
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-02-13 13:11:53 +0100

    List all rows of the table "first_table" from the database "hbtn_0c_0" in your MySQL server

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,4 +1,4 @@
 -- Task 6. List all in table
--- This lists all rows of the table "first_table"
+-- This Script lists all rows of the table "first_table"
 -- from the database "hbtn_0c_0" in your MySQL server
 SELECT * FROM `first_table`;
