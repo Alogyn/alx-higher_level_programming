a11f605fde3b3e7d0a4612a03935ee17c9e16d70
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-02-13 13:23:44 +0100

    Create a table "second_table" in the database "hbtn_0c_0" in your MySQL server and add multiples rows

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,7 +3,9 @@
 -- in the database "hbtn_0c_0" in your MySQL server
 -- and add multiples rows
 CREATE TABLE IF NOT EXISTS `second_table` (
-    `id` INT, `name` VARCHAR(256), `score` INT
+    `id` INT,
+    `name` VARCHAR(256),
+    `score` INT
 );
 INSERT INTO second_table (id, name, score) VALUES
 (1, 'John', 10),
