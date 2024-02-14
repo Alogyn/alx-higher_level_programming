cad3b63f2d9a32bbe8028d886e3f04406771a163
Mohamed Derfoufi <138708102+Alogyn@users.noreply.github.com>
2024-02-14 01:55:47 +0100

    Temperatures Table

--- 0x0D-SQL_introduction/temperatures.sql
+++ 0x0D-SQL_introduction/temperatures.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 5.5.54, for debian-linux-gnu (x86_64
+-- MySQL dump 10.13  Distrib 5.5.54, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: hbtn_0c_0
 -- ------------------------------------------------------
