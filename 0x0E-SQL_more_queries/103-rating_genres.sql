8ce51c5ec3f404d1a337136926a9ef9dc4ebc5f2
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-02-15 01:41:44 +0100

    List all genres in the database hbtn_0d_tvshows_rate by their rating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,9 +1,8 @@
 -- Task 20. Best genre (Advanced)
 -- This script that lists all genres in the database "hbtn_0d_tvshows_rate" by their rating
-SELECT tg.name, SUM(tr.rating) AS rating
-FROM tv_genres tg
-JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
-JOIN tv_shows ts ON tsg.show_id = ts.id
-JOIN tv_show_ratings tr ON ts.id = tr.show_id
-GROUP BY tg.name
-ORDER BY rating DESC;
+SELECT `name`, SUM(`rate`) AS `rating`
+FROM `tv_genres` AS g
+     INNER JOIN `tv_show_genres` AS s ON s.`genre_id` = g.`id`
+     INNER JOIN `tv_show_ratings` AS r ON r.`show_id` = s.`show_id`
+GROUP BY `name`
+ORDER BY `rating` DESC;
