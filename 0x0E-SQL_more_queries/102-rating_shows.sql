91ec18b8263ee8d130c02631b3f12a26ac7583f6
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-02-15 01:31:20 +0100

    List all shows without the genre Comedy in the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,8 @@
 -- Task 19. Rotten tomatoes (Advanced)
 -- This script that lists all shows from "hbtn_0d_tvshows_rate" by their rating
-SELECT tv_shows.title, SUM(rating) AS rating_sum
-FROM tv_shows
-JOIN tv_show_rating ON tv_shows.id = tv_show_rating.show_id
-GROUP BY tv_shows.title
-ORDER BY rating_sum DESC, tv_shows.title ASC;
+SELECT `title`, SUM(`rate`) AS `rating`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_ratings` AS r
+       ON t.`id` = r.`show_id`
+ GROUP BY `title`
+ ORDER BY `rating` DESC;
