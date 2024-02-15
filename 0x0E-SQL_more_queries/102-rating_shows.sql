662130456c4683194b8e2716511ba8bc63ac5afe
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-02-15 01:43:08 +0100

    List all shows from hbtn_0d_tvshows_rate by their rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,7 @@
 -- Task 19. Rotten tomatoes (Advanced)
 -- This script that lists all shows from "hbtn_0d_tvshows_rate" by their rating
 SELECT `title`, SUM(`rate`) AS `rating`
-  FROM `tv_shows` AS t
-       INNER JOIN `tv_show_ratings` AS r
-       ON t.`id` = r.`show_id`
- GROUP BY `title`
- ORDER BY `rating` DESC;
+FROM `tv_shows` AS t
+     INNER JOIN `tv_show_ratings` AS r ON t.`id` = r.`show_id`
+GROUP BY `title`
+ORDER BY `rating` DESC;
