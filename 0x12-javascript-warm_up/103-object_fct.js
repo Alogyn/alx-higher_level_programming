12b0d9eee4b442cee7b936fef3ba5d1e7ae914f4
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-03-12 01:40:55 +0000

    Increment the integer value

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -3,13 +3,13 @@ const myObject = {
   type: 'object',
   value: 12
 };
-  console.log(myObject);
-  myObject.incr = function () {
-    this.value++;
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
 };
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
