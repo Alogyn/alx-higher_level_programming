02540d3724fe2cba3f15fa74c8a72e00207cc06f
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-04-25 16:35:09 -0700

    Find a peak in a list of unsorted integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""Script to find a peak in a list of unsorted integers"""
+""" Script to find a peak in a list of unsorted integers """
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in list_of_integers"""
-
-    if list_of_integers is None or list_of_integers == []:
+    """ Finds a peak in a list of unsorted integers """
+    if not list_of_integers:
         return None
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+
+    def binary_search(arr, low, high):
+        """ Helper function to perform binary search for a peak element """
+        if low == high:
+            return arr[low]
+
+        mid = (low + high) // 2
+        if (mid == 0 or arr[mid - 1] <= arr[mid]) and (mid == len(arr) - 1 or arr[mid + 1] <= arr[mid]):
+            return arr[mid]
+        elif mid > 0 and arr[mid - 1] > arr[mid]:
+            return binary_search(arr, low, mid - 1)
+        else:
+            return binary_search(arr, mid + 1, high)
+
+    return binary_search(list_of_integers, 0, len(list_of_integers) - 1)
