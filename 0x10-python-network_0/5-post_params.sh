c08f167ac92ebd0cd3b12e8d6e7d3e9b2ada3630
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-04-25 16:28:57 -0700

    Take in a URL, sends a POST request to the passed URL, and displays the body of the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script to send a POST request with parameters and display the response body
+# Script to send a POST request with parameters and display the response bod
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
