5dd4862afe321b19a2b46d40f6058af2364790e9
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-04-26 14:03:41 -0700

    Fetches a URL using urllib and prints out the body of the response

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Fetches a URL using urllib and prints out the body of the response """
+
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode('utf-8'))
