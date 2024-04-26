b0263275a16e26efa430e1860c189c5180177587
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-04-26 14:27:55 -0700

    Fetches https://alx-intranet.hbtn.io/status using the requests package and prints the body of the response

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,10 +7,8 @@ prints the body of the response
 
 import requests
 
-if __name__ == "__main__":
-    response = requests.get('https://alx-intranet.hbtn.io/status')
-    content = response.text
-
+if __name__ == '__main__':
+    html = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print(f"    - type: {type(content)}")
-    print(f"    - content: {content}")
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
