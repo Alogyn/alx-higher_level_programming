9d6f0eedd61f218ec6a025f625059d06248057ea
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-04-26 14:09:54 -0700

    Sends a POST request to a specified URL with an email parameter and displays the response body

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to a specified URL with an email parameter and displays the response body.
+"""
+
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': email}).encode('ascii')
+
+    with urllib.request.urlopen(url, data) as response:
+        print(response.read().decode('utf-8'))
